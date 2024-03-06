03ed5e6c058bb76a04c10e801d7a4263448c6a2a
Deepak Swami <dswami1501@outlook.com>
2024-03-06 16:13:27 +0530

    Added junit5 project and added simple test

---
+++ JUnitTesting/my-junit/src/main/java/com/myjunit/myjunit/mycode/MathUtils.java
@@ -0,0 +1,7 @@
+package com.myjunit.myjunit.mycode;
+
+public class MathUtils {
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ JUnitTesting/my-junit/src/test/java/com/myjunit/myjunit/mycode/MathUtilsTest.java
@@ -0,0 +1,11 @@
+package com.myjunit.myjunit.mycode;
+
+import org.junit.jupiter.api.Test;
+
+public class MathUtilsTest {
+
+    @Test
+    void test(){
+        System.out.println("This test run");
+    }
+}
