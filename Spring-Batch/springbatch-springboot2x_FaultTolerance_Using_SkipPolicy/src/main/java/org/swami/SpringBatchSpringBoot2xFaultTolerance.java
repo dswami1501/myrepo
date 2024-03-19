7aa6b03d587b5f47dfbac65971af23c96549d317
Deepak Swami <dswami1501@outlook.com>
2024-03-19 19:09:29 +0530

    Added spring batch fault tolerance project

---
+++ Spring-Batch/springbatch-springboot2x_FaultTolerance_Using_SkipPolicy/src/main/java/org/swami/SpringBatchSpringBoot2xFaultTolerance.java
@@ -0,0 +1,11 @@
+package org.swami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchSpringBoot2xFaultTolerance {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchSpringBoot2xFaultTolerance.class, args);
+    }
+}
---
+++ Spring-Batch/springbatch-springboot2x_FaultTolerance_Using_SkipPolicy/src/main/java/org/swami/config/CustomerProcessor.java
@@ -0,0 +1,15 @@
+package org.swami.config;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.swami.entity.Customer;
+
+public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
+    @Override
+    public Customer process(Customer customer) {
+        int age = Integer.parseInt(customer.getAge());//vhjkdfh38497infdhg
+        if (age >= 18) {
+            return customer;
+        }
+        return null;
+    }
+}
